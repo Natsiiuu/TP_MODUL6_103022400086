3b0c9bf6a139d9a48c142cdfb7e218f4fdc9158f
Natsiiuu <khalifafattan05@.com>
2026-04-06 01:32:18 +0700

    menambahkan implementasi design by contract

--- TP_MODUL6_103022400086/Program.cs
+++ TP_MODUL6_103022400086/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using TP_MODUL6_103022400086;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            SayaMusicTrack lagu1 = new SayaMusicTrack("What Is Love - Twice");
+            lagu1.IncreasePlayCount(100);
+            lagu1.PrintTrackDetails();
+
+            SayaMusicTrack laguError = new SayaMusicTrack(new string('A', 101));
+
+            SayaMusicTrack lagu2 = new SayaMusicTrack("Overflow Test");
+            for (int i = 0; i < 100; i++)
+            {
+                lagu2.IncreasePlayCount(int.MaxValue);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Terjadi error: " + e.Message);
+        }
+    }
+}
